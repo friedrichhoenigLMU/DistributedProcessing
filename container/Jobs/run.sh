7e90b7e5eb6cd6d5f63ff077d41b15d4e18f5b6c
ILIJAXPS\ivukotic <ivukotic@cern.ch>
2018-01-25 23:36:55 -0600

    starting with panda jobs

---
+++ container/Jobs/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "  *******************************  importing jobs table  *******************************"
+
+cd /afs/cern.ch/user/i/ivukotic/ATLAS-Hadoop/import/jobs/
+
+startDate=$(date -u '+%Y-%m-%d %H:00:00' -d "-2hour")
+endDate=$(date -u '+%Y-%m-%d %H:00:00' -d "-1hour")
+fileName=$(date -u '+%Y-%m-%d_%H' -d "-2hour")
+ind=$(date -u '+%Y-%m-%d' -d "-2hour")
+echo "start date: ${startDate}"
+echo "end date: ${endDate}"
+echo "file name: ${fileName}"
+echo "index : ${ind}"
+./JobSqoopToAnalytix.sh "${startDate}" "${endDate}" "${fileName}"
+echo "Sqooping DONE."
+
+pig -4 log4j.properties -f JobsToESuc.pig -param INPD=${fileName} -param ININD=${ind}
+echo "Indexing UC DONE."
