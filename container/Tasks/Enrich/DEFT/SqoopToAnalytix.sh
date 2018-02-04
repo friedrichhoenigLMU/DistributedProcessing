5bab43eed5556472458c911da5211253d656f7ce
ILIJAXPS\ivukotic <ivukotic@cern.ch>
2018-02-04 13:31:00 -0600

    not load tasks with output_formats == null

--- container/Tasks/Enrich/DEFT/SqoopToAnalytix.sh
+++ container/Tasks/Enrich/DEFT/SqoopToAnalytix.sh
@@ -8,7 +8,7 @@ sqoop import \
     -D mapred.child.java.opts="-Djava.security.egd=file:/dev/../dev/urandom" \
     --direct \
     --connect $JOB_ORACLE_CONNECTION_STRING \
-    --query "select TASKID, OUTPUT_FORMATS from ATLAS_DEFT.T_PRODUCTION_TASK  WHERE TIMESTAMP > TO_DATE('$1','YYYY-MM-DD HH24:MI:SS')  AND \$CONDITIONS " \
+    --query "select TASKID, OUTPUT_FORMATS from ATLAS_DEFT.T_PRODUCTION_TASK  WHERE TIMESTAMP > TO_DATE('$1','YYYY-MM-DD HH24:MI:SS') AND OUTPUT_FORMATS IS NOT NULL AND \$CONDITIONS " \
     --username $JOB_ORACLE_USER --password $JOB_ORACLE_PASS \
     -m 1 --target-dir /atlas/analytics/DEFT_temp \
     --map-column-java TASKID=Long,OUTPUT_FORMATS=String
