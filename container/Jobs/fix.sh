b3c9b46996abad6932cadc905abdd04c52c20518
ILIJAXPS\ivukotic <ivukotic@cern.ch>
2018-02-15 16:25:48 -0600

    job fix fix

--- container/Jobs/fix.sh
+++ container/Jobs/fix.sh
@@ -3,6 +3,11 @@
 # eg. fix.sh 2018-01-01
 echo "  *******************************  fixing jobs   *******************************"
 
+export SQOOP_HOME=/usr/local/sqoop
+export PATH=$PATH:$SQOOP_HOME/bin
+export HADOOP_OPTS=-Djava.security.egd=file:/dev/../dev/urandom
+kinit analyticssvc@CERN.CH -k -t /tmp/keytab/analyticssvc.keytab
+
 startDate="$1 00:00:00"
 endDate="$1 23:59:59"
 fileName="$1"
@@ -13,7 +18,7 @@ echo "file name: ${fileName}"
 echo "index : ${ind}"
 
 echo "Removing previous data in HDFS"
-hdfs dfs rm -R -f -skipTrash  hdfs://analytix/atlas/analytics/jobs/$1_*
+hdfs dfs -rm -R -f -skipTrash  hdfs://analytix/atlas/analytics/jobs/$1_*
 
 ./JobSqoopToAnalytix.sh "${startDate}" "${endDate}" "${fileName}"
 echo "Sqooping DONE."
